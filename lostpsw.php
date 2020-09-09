abca58d2a4f81387b3540095d2b0356702dcfc99
LBuilder <2099625@talnet.nl>
2020-09-09 14:37:22 +0200

    test2

---
+++ lostpsw.php
@@ -0,0 +1,8 @@
+<form>
+
+
+            <label> Enter your E-mail to get a new password sended : </label>
+            <input type="E-mail" placeholder="Enter E-mail" name="E-Mail" require>
+
+            <button type="submit">Be a LS Member..</button>   
+</form>
---
+++ signup.php
@@ -0,0 +1,30 @@
+
+<form>  
+        <div class="container">   
+            <label>First-Name : </label>   
+            <input type="text" placeholder="Enter Firstname" name="Firstname" required>
+            
+            <label>Between-Name : </label>   
+            <input type="text" placeholder="Enter Betweenname" name="Betweenname" required> 
+            
+            <label>Last-Name : </label>   
+            <input type="text" placeholder="Enter Lastname" name="Lastname" optional> 
+         
+            <label> E-mail : </label>
+            <input type="E-mail" placeholder="Enter E-mail" name="E-Mail" require>
+
+            <label>User-Name : </label>   
+            <input type="text" placeholder="username" name="username" required>
+            
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required>  
+            
+            <label>Enter Password again : </label>   
+            <input type="password" placeholder="Enter again Password" name="password" required>  
+            
+
+            <button type="submit">Be a LS Member..</button>   
+            <input type="checkbox" checked="checked"> i accept all the rules
+           
+        </div>   
+    </form>     
