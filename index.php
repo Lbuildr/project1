e0e4b6a4901365e4232a4cdc4e0613302cc253ff
LBuilder <2099625@talnet.nl>
2020-09-09 14:34:16 +0200

    test1

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<form>  
+        <div class="container">   
+            <label>Username : </label>   
+            <input type="text" placeholder="Enter Username" name="username" required>  
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required>  
+            <button type="submit">Login</button>   
+            <input type="checkbox" checked="checked"> Remember me   
+            Forgot <a href="#"> password? </a>  
+            No account?! <a href="#"> Klik here..</a> 
+        </div>   
+    </form>     
