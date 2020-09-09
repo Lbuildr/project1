ca4116fbf845f5bcb2b3719f2ceb713150f6b5d5
Lbuildr <54835112+Lbuildr@users.noreply.github.com>
2020-09-09 15:02:02 +0200

    Update lostpsw.php

--- lostpsw.php
+++ lostpsw.php
@@ -4,5 +4,5 @@
             <label> Enter your E-mail to get a new password sended : </label>
             <input type="E-mail" placeholder="Enter E-mail" name="E-Mail" require>
 
-            <button type="submit">Be a LS Member..</button>   
+            <button type="submit">ask new password</button>   
 </form>
