911f4dc79c4a0e4e00942fbc2f65f2471deee38a
LBuilder <2099625@talnet.nl>
2020-09-13 21:00:19 +0200

    test 5

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <form>  
         <div class="container">   
             <label>Username : </label>   
@@ -6,8 +5,11 @@
             <label>Password : </label>   
             <input type="password" placeholder="Enter Password" name="password" required>  
             <button type="submit">Login</button>   
-            <input type="checkbox" checked="checked"> Remember me   
-            Forgot <a href="#"> password? </a>  
-            No account?! <a href="#"> Klik here..</a> 
+            <input type="checkbox" checked="checked"> Remember me  
+            <input type="checkbox" checked="checked"> i accept all the term en conditions 
+            <div  class="button">
+            <button type="Restore Password" formaction="lostpsw.php" formnovalidate>Restore Password</button>
+            </div>
+            No account?! <a href="signup.php"> Klik here..</a> 
         </div>   
     </form>     
